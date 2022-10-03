aaaaa7c63bfbbd64cd4cef5985ca6864f3f484d6
CSFelix <csfelix08@gmail.com>
2022-10-03 20:42:25 -0300

    Updates

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,73 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
 
-export default function cart() {
-  return <div />;
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+
+      <ProductTable>
+        <thead>
+          <tr>
+            <th>-</th>
+            <th>Product</th>
+            <th>Amount</th>
+            <th>Value</th>
+            <th>-</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU'
+                alt='shoes'
+              />
+            </td>
+
+            <td>
+              <strong>Nike Low Dunk</strong>
+              <span>U$50.00</span>
+            </td>
+
+            <td>
+              <div>
+                <button type='button'>
+                  <MdRemoveCircleOutline size={20} coloor='#7159c1' />
+                </button>
+
+                <input type="number" readOnly value={2} />
+
+                <button type='button'>
+                  <MdAddCircleOutline size={20} coloor='#7159c1' />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>U$100.00</strong>
+            </td>
+
+            <td>
+              <button type='button'>
+                <MdDelete size={20} color='7159c1' />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type='button'>Finish Shop!</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>U$250.00</strong>
+        </Total>
+      </footer>
+
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,121 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 
+import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
-export default function home() {
-  return (
-    <ProductList>
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black - Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
+export default class Home extends Component {
+  constructor() {
+    super();
 
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
+    this.state = {
+      products: [],
+    };
+  }
 
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products');
 
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
+    this.setState({ products: data });
+  }
 
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
+  render () {
+    const { products } = this.state;
 
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
+    return (
+      <ProductList>
+        { products.map(product => (
+          <li key={ product.id }>
+            <img src={ product.image } alt={ product.title } />
+            <strong>{ product.title }</strong>
+            <span>{ product.priceFormatted }</span>
 
+            {/** add to cart button */}
+            <button type='button'>
+              <div>
+                <MdAddShoppingCart size={16} color='#fff' /> 3
+              </div>
 
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
-
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
-
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
-
-
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
-
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
-
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
-
-
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
-
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
-
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
-
-
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
-
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
-
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
-
-
-      <li>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4cEC3GQ5JqAUp5_pFDHjL3c0Cgdw4Q8ihw&usqp=CAU' alt='shoes' />
-        <strong>Nike Low Dunk - White and Black</strong>
-        <span>U$ 50.00</span>
-
-        {/** add to cart button */}
-        <button type='button'>
-          <div>
-            <MdAddShoppingCart size={16} color='#fff' /> 3
-          </div>
-
-          <span>Add in the Cart!</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>Add in the Cart!</span>
+            </button>
+          </li>
+        )) }
+      </ProductList>
+    );
+  }
 }
